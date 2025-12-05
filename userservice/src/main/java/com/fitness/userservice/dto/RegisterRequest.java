327750dacef38838def7392cb339f5bd54fb6071
Nishu Kumari <sunshine152018@gmail.com>
2025-12-05 19:49:38 +0530

    First commit

---
+++ userservice/src/main/java/com/fitness/userservice/dto/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.fitness.userservice.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.jspecify.annotations.Nullable;
+
+@Data
+public class RegisterRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be of atleast 6 characters")
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+}
